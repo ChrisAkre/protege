9847f78ceae7a3162241170003ad67c3efb8a5e7
ChrisAkre <256899919+ChrisAkre@users.noreply.github.com>
2026-05-14 23:01:55 -0400

    🧹 Remove unused import org.junit.jupiter.api.Disabled (#112)

--- src/it/hibernate-test/src/test/java/dev/akre/it/hibernate/HibernateTest.java
+++ src/it/hibernate-test/src/test/java/dev/akre/it/hibernate/HibernateTest.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
